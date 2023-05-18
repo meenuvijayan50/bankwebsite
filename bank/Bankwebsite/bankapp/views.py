98cb56c121fde8cb26518cce938635d351f5e11e
MEENU VIJAYAN <119869016+meenuvijayan50@users.noreply.github.com>
2023-05-18 13:41:20 +0530

    Update views.py

--- bank/Bankwebsite/bankapp/views.py
+++ bank/Bankwebsite/bankapp/views.py
@@ -1,19 +1,18 @@
-from django.contrib import auth, messages
-from django.contrib.auth.models import User
-from django.http import HttpResponse
 from django.contrib import messages, auth
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
 
-from bankapp.forms import UserForm
-from bankapp.models import District,Branches
+
+from .forms import PersonCreationForm
+from .models import Person, City
 
 
 # Create your views here.
 def demo(request):
     return render(request,"base.html")
-def login(request):
+def log(request):
     if request.method== 'POST':
         username=request.POST['username']
         password = request.POST['password']
@@ -23,31 +22,40 @@ def login(request):
             return redirect('/')
         else:
             messages.info(request,"invalid credentials")
-            return redirect('login')
+            # return redirect('login')
     return render(request, "login.html")
+
 
 def register(request):
     return render(request,"register.html")
 
-def userform(request):
-    District_id =request.POST.get('District')
-    branche= Branches.objects.filter(District_id=District_id).order_by('name')
-    return render(request,"forms.html" , {'branche' : branche})
-
-
-def add_user(request):
-    form = UserForm()
+def person_create_view(request):
+    form = PersonCreationForm()
     if request.method == 'POST':
-        form = UserForm(request.POST)
+        form = PersonCreationForm(request.POST)
         if form.is_valid():
-            # form.save()
+            form.save()
             messages.info(request, " submitted successfully.")
-            # return redirect('register')
-    return render(request, 'branches.html', {'form': form})
+            form = PersonCreationForm()
 
-def load_branches(request):
-    District_id = request.GET.get('District_id')
-    branches= User.objects.filter(District_id=District_id).all()
-    return render(request, 'forms.html', {'branches': branches})
-# def submit(request):
-#     return HttpResponse("Application accepted")
+    return render(request, 'home.html', {'form': form})
+
+
+def person_update_view(request, pk):
+    person = get_object_or_404(Person, pk=pk)
+    form = PersonCreationForm(instance=person)
+    if request.method == 'POST':
+        form = PersonCreationForm(request.POST, instance=person)
+        if form.is_valid():
+            form.save()
+            return redirect('person_change', pk=pk)
+    return render(request, 'home.html', {'form': form})
+
+
+# AJAX
+def load_cities(request):
+    district_id = request.GET.get('district_id')
+    cities = City.objects.filter(district_id=district_id).all()
+    return render(request, 'forms.html', {'cities': cities})
+    # return JsonResponse(list(cities.values('id', 'name')), safe=False)
+
